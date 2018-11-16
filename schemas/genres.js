ed5c4b057b71d89179af6687bcb4a52d31587719
Wilton Ngo <wiltonngo@gmail.com>
2018-11-16 14:07:13 -0500

    Updated genres endpoints

--- schemas/genres.js
+++ schemas/genres.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const genresSchema = new mongoose.Schema({
-    name: String
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20
+    }
 });
 
 module.exports = genresSchema;
