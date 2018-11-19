743333248b06aeb34d6819c30dd2a18b8583dd8f
Wilton Ngo <wiltonngo@gmail.com>
2018-11-18 23:04:43 -0500

    - Separated genre code into exportable schema and model

--- models/genre.js
+++ models/genre.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const Genre = mongoose.model('genre', mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            minlength: 3,
-            maxlength: 20
-        }
-    }
-));
 
-module.exports = Genre;
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20
+    }
+});
+
+const Genre = mongoose.model('genre', genreSchema);
+
+module.exports.genreSchema = genreSchema;
+module.exports.Genre = Genre;
