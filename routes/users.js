cdfc95f8fdfe522fc8899b519a654c64df2c6f6c
Wilton Ngo <wiltonngo@gmail.com>
2018-11-20 16:22:53 -0500

    Added password hashing to registerUser()'

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const router = express.Router();
 
@@ -28,10 +29,13 @@ async function registerUser(req, res) {
   try {
     let user = await User.findOne({email: req.body.email});
     if (user) return res.status(409).send('The specified account already exists.');
-    user = _.pick(req.body, ['name', 'password', 'email']);
-    await user.save();
+    
+    user = new User(_.pick(req.body, ['name', 'password', 'email']));
+    let salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save();
 
-    return res.status(200).send(_.pick(user, ['name', 'email']));
+    return res.status(200).send(_.pick(user, ['name', 'password', 'email']));
   } catch (error) {
     return res.send(error);
   }
