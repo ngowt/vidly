189d9e9188d8b0fd7c02bb586098d8df6bd62449
Wilton Ngo <wiltonngo@gmail.com>
2018-11-22 12:23:33 -0500

    Added generateAuthToken() function to user schema

--- models/user.js
+++ models/user.js
@@ -1,29 +1,36 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const User = mongoose.model('user', mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 1,
-            maxlength: 255
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 5,
-            maxlength: 1024
-        },  
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true,
-            min: 1,
-            max: 255
-        }
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },  
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        min: 1,
+        max: 255
     }
-));
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({_id: this._id}, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
