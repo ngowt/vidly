985a6bcb7069f3b4153f47140790f4d8b000a8e2
Wilton Ngo <wiltonngo@gmail.com>
2018-11-24 17:45:08 -0500

    Unindentation

--- routes/customers.js
+++ routes/customers.js
@@ -18,41 +18,41 @@ router.put('/:id', [auth, admin], asyncMiddleware(updateCustomer));
 router.delete('/:id', [auth, admin], asyncMiddleware(removeCustomer));
 
 async function removeCustomer(req, res) {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) { return res.status(400).send('Invalid customer') };
-    const result = await Customer.findByIdAndRemove(req.params.id);
-    if (!result) { return res.status(404).send('The customer with the given ID was not found.')};
-    return res.status(200).send(result);
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) { return res.status(400).send('Invalid customer') };
+  const result = await Customer.findByIdAndRemove(req.params.id);
+  if (!result) { return res.status(404).send('The customer with the given ID was not found.')};
+  return res.status(200).send(result);
 }
 
 async function getCustomer(req, res) {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) { return res.status(400).send('Invalid customer') };
-    const customer = await Customer.findById(req.params.id);
-    if (!customer) { return res.status(404).send('The customer with the given ID was not found.')};
-    return res.status(200).send(customer);
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) { return res.status(400).send('Invalid customer') };
+  const customer = await Customer.findById(req.params.id);
+  if (!customer) { return res.status(404).send('The customer with the given ID was not found.')};
+  return res.status(200).send(customer);
 }
 
 async function getCustomers(req, res) {
-    const customer = await Customer
-      .find()
-      .sort({name: 1});
-    return res.status(200).send(customer);
+  const customer = await Customer
+    .find()
+    .sort({name: 1});
+  return res.status(200).send(customer);
 }
 
 async function insertCustomer(req, res) {
-    let newCustomer = new Customer(_.pick(req.body, ['name', 'isGold', 'phone']));
-    await newCustomer.save();   
-    return res.status(200).send(newCustomer);
+  let newCustomer = new Customer(_.pick(req.body, ['name', 'isGold', 'phone']));
+  await newCustomer.save();   
+  return res.status(200).send(newCustomer);
 }
 
 async function updateCustomer(req, res) {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) { return res.status(400).send('Invalid customer') };
-    const result = await Customer.findById(req.params.id);
-    if (!result) { return res.status(404).send('The customer with the given ID was not found.') };
-    for (var key in req.body) {
-      result[key] = req.body[key];
-    }
-    await result.save();
-    return res.status(200).send(result);
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) { return res.status(400).send('Invalid customer') };
+  const result = await Customer.findById(req.params.id);
+  if (!result) { return res.status(404).send('The customer with the given ID was not found.') };
+  for (var key in req.body) {
+    result[key] = req.body[key];
+  }
+  await result.save();
+  return res.status(200).send(result);
 }
 
 module.exports = router;
