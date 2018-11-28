8e78aab2762ca4aed85f1768147bd9b24c53d507
Wilton Ngo <wiltonngo@gmail.com>
2018-11-28 00:56:08 -0500

    Added additional integration tests

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
+const User = require('../../models/user');
 
 describe('/api/genres', () => {
     beforeEach( () => { server = require('../../index'); });
@@ -35,6 +36,60 @@ describe('/api/genres', () => {
         it('should throw a 400 error with an invalid objectid', async () => {
             const res = await request(server).get(`/api/genres/1111`);
             expect(res.status).toBe(400);
+        });
+    });
+
+    describe('POST /', () => {
+        beforeEach( () => {
+            token = new User().generateAuthToken();
+            genreName = 'fiction';
+        });
+
+        // Define the happy path, and then in each test, we change 
+        // one parameter that clearly aligns with the name of the test
+        
+        let token;
+        let genreName;
+
+        const exec = async () => {
+            const res = await request(server)
+                .post('/api/genres')
+                .set('x-auth-token', token)
+                .send({ name: genreName });
+            return res;
+        };
+        
+        it('should return 401 if client is not logged in', async () => {
+            token = '';
+            const res = await exec();
+            expect(res.status).toBe(401);
+        });
+
+        it('should return 400 if genre is < 3 characters', async () => {
+            genreName = 'aa';
+            const res = await exec();
+            expect (res.status).toBe(400);
+        });
+
+        it('should return 400 if genre is > 20 characters', async () => {
+            genreName = new Array(22).join('a');
+            const res = await exec();
+            expect(res.status).toBe(400);
+        });
+
+        it('should save the genre if valid', async () => {
+            genreName = 'fiction'
+            const res = await exec();
+            const genre = Genre.find({ name: genreName});
+            expect(genre).not.toBeNull();
+            expect(res.status).toBe(200);
+        });
+
+        it('should return the genre if valid', async () => {
+            genreName = 'fiction';
+            const res = await exec();
+            expect(res.body).toHaveProperty('_id');
+            expect(res.body).toHaveProperty('name', genreName);
         });
     })
 });
