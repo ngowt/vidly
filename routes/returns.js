2e5769f767ca297cdef18f105c8714f31701dcc1
Wilton Ngo <wiltonngo@gmail.com>
2018-11-30 12:50:53 -0500

    Added customerid and movieid req.body check

--- routes/returns.js
+++ routes/returns.js
@@ -7,7 +7,12 @@ const router = express.Router();
 router.post('/', auth, asyncMiddleware(insertReturn));
 
 async function insertReturn(req, res) {
-    return res.status(200).send();
+    if (!req.body.customerId) return res.status(400).send('Invalid customerId');
+    if (!req.body.movieId) return res.status(400).send('Invalid movieId');
+    return res.status(200).send({
+        customerId: req.body.customerId,
+        movieId: req.body.movieId
+    });
 }
 
 module.exports = router;
